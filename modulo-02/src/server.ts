13fa9a50c3a83c031c5ef32a60571a58fc821b66
Davi <inovacao@controller-rnc.com.br>
2025-04-30 17:50:50 -0300

    commit

--- modulo-02/src/server.ts
+++ modulo-02/src/server.ts
@@ -1,17 +1,30 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transacao teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return transactions
+})
+
+app.get('/get', async () => {
+  const transactions = await knex('transactions').select('*')
+  return transactions
 })
 
 app
   .listen({
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on http://localhost:' + env.PORT)
   })
