e757789291b124e11704d0770fdfe96e2903582c
Davi <inovacao@controller-rnc.com.br>
2025-05-16 17:17:59 -0300

    commit

--- modulo-03/src/app.ts
+++ modulo-03/src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'alice@email',
+  },
+})
 
 export const app = fastify()
